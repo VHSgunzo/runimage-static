eff2c320637522bc36134aec38e162a9e20a03e2
VHSgunzo <wosganzo@gmail.com>
2023-06-28 11:35:33 +0300

    v0.39.1

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 cd "$(dirname "$(readlink -f "$0" 2>/dev/null)" 2>/dev/null)"
+echo "= git lfs install"
+git lfs install
 echo "= git lfs pull"
 git lfs pull
 echo "= create archlinux package"
